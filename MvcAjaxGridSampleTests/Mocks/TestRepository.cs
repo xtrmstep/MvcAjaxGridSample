33b66afaee79030d43ed2441e4f4fd07c4bd51ab
Alexander Goida <alex.goida@xtrmstep.ru>
2014-09-12 00:29:02 +0400

    Test are fixed

--- MvcAjaxGridSampleTests/Mocks/TestRepository.cs
+++ MvcAjaxGridSampleTests/Mocks/TestRepository.cs
@@ -7,7 +7,8 @@ namespace MvcAjaxGridSampleTests.Mocks
     {
         public TestRepository()
         {
-            _internalStorage.Add(1, new Book {Id = 1, Title = "Book 1", IssueYear = 2001});
+            _internalStorage.Clear();
+            _internalStorage.Add(1, new Book { Id = 1, Title = "Book 1", IssueYear = 2001 });
             _internalStorage.Add(2, new Book { Id = 2, Title = "Book 2", IssueYear = 2002 });
             _internalStorage.Add(3, new Book { Id = 3, Title = "Book 3", IssueYear = 2003 });
             _internalStorage.Add(4, new Book { Id = 4, Title = "Book 4", IssueYear = 2004 });
